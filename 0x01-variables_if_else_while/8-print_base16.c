5dfd2494960487c9ba4bda2329d04a0322842834
Grug23 <essostephen929@gmail.com>
2023-03-20 21:01:00 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints number and letters between a to f.
+ *
+ * Return: Alwayws 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (i = 97; i < 103; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
