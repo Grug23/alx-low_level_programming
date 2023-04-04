36b8d2abe19a85866969d64b5f53bf6e9569f451
Grug23 <essostephen929@gmail.com>
2023-04-03 22:46:06 -0700

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - entry point
+ * @s: input
+ * @accept: input
+ * Return: always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+		if (*s == accept[i])
+		return (s);
+		}
+	s++;
+	}
+return ('\0');
+}
